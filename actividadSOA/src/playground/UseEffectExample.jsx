8a1334b7ba8246bb5983f8c68caff92ce8ff9207
leogeney <ljgeneyl@ufpso.edu.co>
2026-03-10 13:18:19 -0500

    useEffect ready!

--- actividadSOA/src/playground/UseEffectExample.jsx
+++ actividadSOA/src/playground/UseEffectExample.jsx
@@ -1,7 +1,35 @@
-function UseEffectExample() {
+import { useState, useEffect } from "react";
+
+function UseEffectExample(){
+
+  const [nombre, setNombre] = useState("");
+
+  function changeTitle(){
+    document.title = nombre;
+  }
+
+  useEffect(() => {
+    changeTitle();
+  }, [nombre]);
+
+  function change(e){
+
+    setNombre(e.target.value);
+  }
+
   return (
     <div>
-      <h1>Ejemplo de useEffect</h1>
+
+      <h1>Use Effect (Cambia el titulo)</h1>
+
+      <input className="inputEffect"
+        type="text"
+        placeholder="Escribe tu nombre"
+        onChange={change}
+      />
+
+      <p className="holaEffect">Hola {nombre}</p>
+
     </div>
   );
 }
